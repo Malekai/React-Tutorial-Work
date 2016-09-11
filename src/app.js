c54e8ce23c1924d7d94e68e057d27634bc2347f9
Malek <mmalek1421@gmail.com>
2016-09-11 16:55:49 -0400

    Testing

---
+++ src/app.js
@@ -0,0 +1,100 @@
+import 'babel-polyfill';
+import React from 'js/react';
+import ReactDOM from "js/react-dom";
+
+var Comment = React.createClass({
+  getInitialState: function() {
+    return {editing: false}
+  },
+  edit: function () {
+    this.setState({editing: true});
+  },
+  save: function() {
+    this.props.updateCommentText(this.refs.newtext.value, this.props.index);
+    this.setState({editing: false});
+  },
+  remove: function() {
+    this.props.deleteFromBoard(this.props.index);
+  },
+
+  renderNormal: function() {
+    return (
+      <div className="commentContainer">
+        <div className="commentText">{this.props.children}</div>
+        <button onClick={this.edit} className="button-primary">Edit</button>
+        <button onClick={this.remove} className="button-danger">Remove</button>
+      </div>
+    );
+  },
+
+  renderForm: function() {
+    return (
+      <div className="commentContainer">
+        <textarea ref="newtext" defaultValue={this.props.children}></textarea>
+        <button onClick={this.save} className="button-primary">Save</button>
+      </div>
+    );
+  },
+
+  render: function() {
+    if (this.state.editing) {
+      return this.renderForm();
+    } else {
+      return this.renderNormal();
+    }
+  }
+});
+
+//manager for all comments
+var Board = React.createClass({
+
+  getInitialState: function() {
+    return {
+      comments: []
+    }
+  },
+
+  add: function(text) {
+    var arr = this.state.comments;
+    arr.push(text);
+    this.setState({comments: arr})
+  },
+
+  removeComment: function(i) {
+    var arr = this.state.comments;
+    arr.splice(i, 1);
+    this.setState({comments: arr})
+  },
+
+  updateComment: function(newtext, i) {
+    var arr = this.state.comments;
+    arr[i] = newtext;
+    this.setState({comments: arr})
+  },
+
+  eachComment: function(text, i) {
+    return (
+      <Comment key={i} index={i} updateCommentText={this.updateComment} deleteFromBoard={this.removeComment}>
+          {text}
+      </Comment>);
+  },
+
+  render: function() {
+    return(
+      <div>
+        <button onClick={this.add.bind(null, 'Tuna')}>Add New</button>
+        <div className="board">
+          {
+            this.state.comments.map(this.eachComment)
+          }
+        </div>
+      </div>
+    );
+  }
+
+});
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById('container')
+);
